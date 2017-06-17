bd017383a6e24f728bdeca82e315398b595ccadb
Alin Jerpelea <alin.jerpelea@sonymobile.com>
2017-06-17 09:14:53 +0200

    add WLAN_DEBUG ifdef to fix the build
    In the relese there were missing ifdef which broke the compilation
    when debug & debug is not enabled
    
    Signed-off-by: Alin Jerpelea <alin.jerpelea@sonymobile.com>

--- core/wma/src/wma_features.c
+++ core/wma/src/wma_features.c
@@ -2318,6 +2318,7 @@ static void wma_send_status_to_suspend_ind(tp_wma_handle wma, bool suspended)
  *
  * Return: reason code in string format
  */
+#ifdef WLAN_DEBUG
 static const u8 *wma_wow_wake_reason_str(A_INT32 wake_reason)
 {
 	switch (wake_reason) {
@@ -2423,6 +2424,7 @@ static const u8 *wma_wow_wake_reason_str(A_INT32 wake_reason)
 		return "unknown";
 	}
 }
+#endif
 
 /**
  * wma_wow_stats_display() - display wow wake up stats
@@ -2750,6 +2752,7 @@ static bool tlv_check_required(int32_t reason)
  *
  * Return: string for proto subtype for data packet
  */
+#ifdef WLAN_DEBUG
 static const char *
 wma_pkt_proto_subtype_to_string(enum qdf_proto_subtype proto_subtype)
 {
@@ -2810,6 +2813,7 @@ wma_pkt_proto_subtype_to_string(enum qdf_proto_subtype proto_subtype)
 		return "Invalid Packet";
 	}
 }
+#endif
 
 /**
  * wma_wow_get_pkt_proto_subtype() - get the proto subtype
@@ -3234,6 +3238,7 @@ exit_handler:
 	wma_beacon_miss_handler(wma, wake_info->vdev_id, 0);
 }
 
+#ifdef WLAN_DEBUG
 static const char *wma_vdev_type_str(uint32_t vdev_type)
 {
 	switch (vdev_type) {
@@ -3255,6 +3260,7 @@ static const char *wma_vdev_type_str(uint32_t vdev_type)
 		return "unknown";
 	}
 }
+#endif
 
 /**
  * wma_wow_wakeup_host_event() - wakeup host event handler
